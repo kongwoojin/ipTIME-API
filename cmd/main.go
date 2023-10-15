0d635fa17cca2231933acb1f9fbec984f108f7aa
WooJin Kong <kongwoojin03@gmail.com>
2023-10-15 13:33:09 +0900

    feat: Write API call test in main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/kongwoojin/ipTIME-API/cmd/routers"
 	"log"
 )
 
+// main function for testing API call
 func main() {
 	router := RouterInfo()
 	client := CreateClient()
 
 	if routers.Login(client, router) {
-		routers.RouterStatus(client, router)
-		routers.GetConnectedClientList(client, router)
+		// Check Router status
+		fmt.Printf("%+v\n", *routers.RouterStatus(client, router))
+
+		// Check connected clients
+		for i, v := range routers.GetConnectedClientList(client, router) {
+			fmt.Printf("%d: %+v\n", i, v)
+		}
 	} else {
 		log.Fatal("Login failed")
 	}
